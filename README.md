fa3143228f33fc103996f39befec9770dccdabe5
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2017-02-04 02:37:00 -0500

    Update readme and append LSTM

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
 # Bi-LSTM-CRF
-Experimental comparative study setup for various LSTM architectures, including:
-
-* Recurrent Neural Networks (RNN)
+Experimental implementation of various mapped many-to-many recurrent networks.
+The repository includes the following different architectures:
 * Vanilla LSTM Architecture
 * Bidirectional LSTM Architecture
 * Bidirectional LSTM + Conditional Random Fields (CRF) Architecture
+
+### Key Implementation Notes
+* The general structure of the text-to-tag model is built from the following structure:
+
+        model = Sequence()
+        model.add(Embedding(max_features, 128, input_length=maxlen))
+        model.add(Bidirectional(LSTM(64, return_sequences=True)))
+        model.add(TimeDistributed(Dense(1, activation='sigmoid')))
+
+        model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
+
+* Note the use of the `TimeDistributed` layer which enables the many-to-many architecture.
+* This architecture will work with variable length texts.
+* The only tricky part of getting this to work is fixing your data dimensions;
+notably your `y_train` and `y_test` must be 3D and needs to be adjusted accordingly.
+    * See https://github.com/fchollet/keras/issues/3916#issuecomment-250689482
+    * See the BiLSTM Example in the repo for how to change dimension.
+    * Just encapsulate each of the integer label in another list [x].
--- lstm.py
+++ lstm.py
@@ -1,6 +1,48 @@
 '''
-Token Level Classification
+Token Classification - Most Basic Configuration
 Using Vanilla LSTM Network Architecture
 
 Author: Yuya Jeremy Ong (yuyajeremyong@gmail.com)
 '''
+from __future__ import print_function
+import numpy as np
+np.random.seed(1337)  # for reproducibility
+
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Embedding, LSTM, Bidirectional, TimeDistributed
+from keras.datasets import imdb
+from keras.utils.np_utils import to_categorical
+
+max_features = 20000
+maxlen = 100  # cut texts after this number of words (among top max_features most common words)
+batch_size = 32
+
+print('Loading data...')
+(X_train, y_train), (X_test, y_test) = imdb.load_data(nb_words=max_features)
+print(len(X_train), 'train sequences')
+print(len(X_test), 'test sequences')
+
+print("Pad sequences (samples x time)")
+X_train = sequence.pad_sequences(X_train, maxlen=maxlen)
+X_test = sequence.pad_sequences(X_test, maxlen=maxlen)
+
+# Generate Arbitrary Binary Annotations - These labels are just for test, use actual data...
+y_train = []
+for x in X_train:
+    y_train.append([[1] if i % 5 == 0 else [0] for i in x])
+
+y_test = []
+for x in X_test:
+    y_test.append([[1] if i % 5 == 0 else [0] for i in x])
+
+model = Sequential()
+model.add(Embedding(max_features, 128, input_length=maxlen))
+model.add(LSTM(64, return_sequences=True))
+model.add(TimeDistributed(Dense(1, activation='sigmoid')))
+model.summary()
+
+model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
+
+print('Train...')
+model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=4, validation_data=[X_test, y_test])
